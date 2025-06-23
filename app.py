2204be69e1a2019d9e88b7911c6a4e188c7990a3
A-K-0802 <aalok.thakkar01@gmail.com>
2025-06-23 16:53:43 +0530

    Bind tools and working on terminal

--- app.py
+++ app.py
@@ -4,22 +4,25 @@ from langchain.prompts import PromptTemplate
 import requests
 import streamlit as st
 import os
-from langchain_community.tools import tool
+from langchain_community.tools import tool, StructuredTool
 from dotenv import load_dotenv
 from langchain.agents import create_react_agent, AgentExecutor
+from langchain_core.output_parsers import StrOutputParser
+from pydantic import BaseModel,Field
+import json
 load_dotenv()
 
 client = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",)
 
-prompt_template= PromptTemplate(
-    template="""You are a helpful AI assistant.
-    Your job is to help the user find a suitable project idea using GitHub repositories.
-    You will be provided by the user about the {topic}, the {language} of the project, and what they {need} you to provide.
-    The need can either be a project idea, example repository, or a list of repositories, or any open source project where the user can contribute.
-    You will use the tools provided to you to help the user.""",
-    input_variables=["topic", "need", "language"]
-)
+# prompt_template= PromptTemplate(
+#     template="""You are a helpful AI assistant.
+#     Your job is to help the user find a suitable project idea using GitHub repositories.
+#     You will be provided by the user about the {topic}, the {language} of the project, and what they {need} you to provide.
+#     The need can either be a project idea, example repository, or a list of repositories, or any open source project where the user can contribute.
+#     You will use the tools provided to you to help the user.""",
+#     input_variables=["topic", "need", "language"]
+# )
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
@@ -41,7 +44,6 @@ def get_git_repo(topic:str, language:str, max_count:int=5):
     
     url="https://api.github.com/search/repositories"
     response = requests.get(url, headers=headers, params=params)
-
     repos= response.json().get("items", [])
 
     llm_data = []
@@ -55,4 +57,18 @@ def get_git_repo(topic:str, language:str, max_count:int=5):
         })
     return llm_data
 
-#print(get_git_repo.invoke({"topic": "machine learning", "language": "python"}))
+# print(get_git_repo.invoke({"topic": "pathfinding", "language": "python"}))
+
+agent= client.bind_tools([get_git_repo])
+
+messages=[HumanMessage("I want to contribute to a project on pathfinding using Python. Can you help me find a open source project after looking at existing repos?")]
+
+response=agent.invoke(messages)
+messages.append(response)
+
+for tool_call in response.tool_calls:
+    tool_response = get_git_repo.invoke(tool_call)
+    messages.append(tool_response)
+
+result=agent.invoke(messages)
+print(result.content)
